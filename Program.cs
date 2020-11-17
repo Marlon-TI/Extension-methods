3b2fc04f8310ed84e7d952961cd3a84889f8255a
Marlon <marlonsmoreira@gmail.com>
2020-11-17 16:13:52 -0300

    Aula

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Extension_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string A = "UMA FRASE QUALQUER COM PALAVRAS!";
+            Console.WriteLine(A.Cut(10));
+        }
+    }
+}
